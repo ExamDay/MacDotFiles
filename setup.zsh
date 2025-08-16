8ca9262cf66044dbae6ee8c9d8fd6624cf2b0b77
Gabe M. LaFond <lafond.gabe@mayo.edu>
2025-08-16 06:09:29 -0700

    Perfect!

---
+++ setup.zsh
@@ -0,0 +1,8 @@
+# Change to the directory where nvim-cmp is installed:
+cd ~/.local/share/nvim/site/pack/packer/start/nvim-cmp
+# Fetch the pull request where the "put menu above the cursor" feature is implemented:
+git fetch origin pull/1701/head:feat/above
+# Switch to the new branch:
+git checkout feat/above
+# Go back to the home directory:
+cd ~
