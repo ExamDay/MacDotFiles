28e40a712cb2f774da886f1c91874cbc6d1cfe73
Gabe M. LaFond <lafond.gabe@mayo.edu>
2025-08-19 13:48:12 -0700

    path completion fixed

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -64,6 +64,7 @@ mapping = {
 
 sources = cmp.config.sources({
   { name = 'nvim_lsp' },
+  { name = 'path' },
   -- { name = 'vsnip' }, -- For vsnip.
   { name = 'luasnip' }, -- For luasnip.
   -- { name = 'ultisnips' }, -- For ultisnips.
@@ -84,24 +85,24 @@ sources = cmp.config.sources({
 })
 require("cmp_git").setup() ]]--
 
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-mapping = cmp.mapping.preset.cmdline(),
-sources = {
-  { name = 'buffer' }
-}
-})
+-- -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline({ '/', '?' }, {
+-- mapping = cmp.mapping.preset.cmdline(),
+-- sources = {
+--   { name = 'buffer' }
+-- }
+-- })
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-mapping = cmp.mapping.preset.cmdline(),
-sources = cmp.config.sources({
-  { name = 'path' }
-}, {
-  { name = 'cmdline' }
-}),
-matching = { disallow_symbol_nonprefix_matching = false }
-})
+-- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(':', {
+-- mapping = cmp.mapping.preset.cmdline(),
+-- sources = cmp.config.sources({
+--   { name = 'path' }
+-- }, {
+--   { name = 'cmdline' }
+-- }),
+-- matching = { disallow_symbol_nonprefix_matching = false }
+-- })
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
